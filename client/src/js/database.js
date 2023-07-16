f745f2de8dc1fd5d4e93161073b4390056a28dab
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-16 18:57:28 +0530

    Retrieve text from indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,7 +23,14 @@ export const putDb = async (content) => {
   console.log(result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+// Retrieve text from indexedDB
+export const getDb = async (e) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  return result?.value;
+};
 
 initdb();
